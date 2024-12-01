4e644e6c1830ba06ea95e1da1db6b52c3b28024d
Matt Mullins <mmullins@mmlx.us>
2024-11-30 22:46:06 -0800

    day 01: parse

--- day_01/src/lib.rs
+++ day_01/src/lib.rs
@@ -3,11 +3,23 @@ mod test;
 
 use prelude::*;
 
-struct Solution();
+struct Solution {
+    left: Vec<u64>,
+    right: Vec<u64>,
+}
 
 impl Day for Solution {
     fn new(input: &str) -> Self {
-        todo!()
+        let mut left = vec![];
+        let mut right = vec![];
+
+        for line in input.lines() {
+            let mut split = line.split_whitespace();
+            left.push(split.next().unwrap().parse().unwrap());
+            right.push(split.next().unwrap().parse().unwrap());
+        }
+
+        Self { left, right }
     }
 
     fn part1(&self) -> anyhow::Result<u64> {
