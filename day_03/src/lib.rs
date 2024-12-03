4ee57bf434376b88da47829012ce2d6df2246e97
Matt Mullins <mmullins@mmlx.us>
2024-12-02 21:07:16 -0800

    day 03: use regexes
    This ... seems to actually match nothing?

--- day_03/src/lib.rs
+++ day_03/src/lib.rs
@@ -3,15 +3,29 @@ use prelude::*;
 #[cfg(test)]
 mod test;
 
-pub struct Solution {}
+pub struct Solution {
+    lines: Vec<String>,
+}
 
 impl Day for Solution {
     fn new(input: &str) -> Self {
-        todo!()
+        Self {
+            lines: input.lines().map(str::to_owned).collect(),
+        }
     }
 
     fn part1(&self) -> anyhow::Result<u64> {
-        todo!()
+        let mul_regex = Regex::new(r"mul\(([0-9]+),([0-9]+)\)").unwrap();
+        Ok(self
+            .lines
+            .iter()
+            .flat_map(|line| {
+                mul_regex.captures_iter(line).map(|c| {
+                    c.get(1).unwrap().as_str().parse::<u64>().unwrap()
+                        * c.get(2).unwrap().as_str().parse::<u64>().unwrap()
+                })
+            })
+            .sum())
     }
 
     fn part2(&self) -> anyhow::Result<u64> {
