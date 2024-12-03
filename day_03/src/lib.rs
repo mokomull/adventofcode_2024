37ca8655e14f708f1fdcded1e27a628cc53fbfe1
Matt Mullins <mmullins@mmlx.us>
2024-12-02 21:44:28 -0800

    day 03: clean up the solution
    Now that I know neither part actually *cares* about the input lines
    being separate, just store one big String.

--- day_03/src/lib.rs
+++ day_03/src/lib.rs
@@ -4,26 +4,23 @@ use prelude::*;
 mod test;
 
 pub struct Solution {
-    lines: Vec<String>,
+    input: String,
 }
 
 impl Day for Solution {
     fn new(input: &str) -> Self {
         Self {
-            lines: input.lines().map(str::to_owned).collect(),
+            input: input.to_owned(),
         }
     }
 
     fn part1(&self) -> anyhow::Result<u64> {
         let mul_regex = Regex::new(r"mul\(([0-9]+),([0-9]+)\)").unwrap();
-        Ok(self
-            .lines
-            .iter()
-            .flat_map(|line| {
-                mul_regex.captures_iter(line).map(|c| {
-                    c.get(1).unwrap().as_str().parse::<u64>().unwrap()
-                        * c.get(2).unwrap().as_str().parse::<u64>().unwrap()
-                })
+        Ok(mul_regex
+            .captures_iter(&self.input)
+            .map(|c| {
+                c.get(1).unwrap().as_str().parse::<u64>().unwrap()
+                    * c.get(2).unwrap().as_str().parse::<u64>().unwrap()
             })
             .sum())
     }
@@ -33,10 +30,8 @@ impl Day for Solution {
         let do_regex = Regex::new(r"do\(\)").unwrap();
         let dont_regex = Regex::new(r"don't\(\)").unwrap();
 
-        let data = self.lines.iter().join("\n");
-
-        let dos = do_regex.find_iter(&data).map(|m| m.start());
-        let donts = dont_regex.find_iter(&data).map(|m| m.end());
+        let dos = do_regex.find_iter(&self.input).map(|m| m.start());
+        let donts = dont_regex.find_iter(&self.input).map(|m| m.end());
 
         let mut do_or_dont = dos
             .map(|i| (i, true))
@@ -53,8 +48,6 @@ impl Day for Solution {
                 (true, true) | (false, false) => {}
                 // was do, but now we should don't
                 (true, false) => {
-                    dbg!((last_start, pos));
-                    dbg!(&data[last_start..pos]);
                     ranges.push(last_start..pos);
                 }
                 // was don't, but now we should do... record the start of this range
@@ -68,13 +61,13 @@ impl Day for Solution {
 
         // and if we ended with a `do`, also add that to ranges
         if last_do {
-            ranges.push(last_start..data.len());
+            ranges.push(last_start..self.input.len());
         }
 
         Ok(ranges
             .into_iter()
             .flat_map(|idxs| {
-                mul_regex.captures_iter(&data[idxs]).map(|c| {
+                mul_regex.captures_iter(&self.input[idxs]).map(|c| {
                     c.get(1).unwrap().as_str().parse::<u64>().unwrap()
                         * c.get(2).unwrap().as_str().parse::<u64>().unwrap()
                 })
