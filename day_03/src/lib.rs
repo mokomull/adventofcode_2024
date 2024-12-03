20bc1ced99f081d72780a1e4376c5f11ed7ac516
Matt Mullins <mmullins@mmlx.us>
2024-12-02 21:23:11 -0800

    day 03: iterator and match soup
    This seems to not work.

--- day_03/src/lib.rs
+++ day_03/src/lib.rs
@@ -29,6 +29,52 @@ impl Day for Solution {
     }
 
     fn part2(&self) -> anyhow::Result<u64> {
-        todo!()
+        let mul_regex = Regex::new(r"mul\(([0-9]+),([0-9]+)\)").unwrap();
+        let do_regex = Regex::new(r"do\(\)").unwrap();
+        let dont_regex = Regex::new(r"don't\(\)").unwrap();
+
+        Ok(self
+            .lines
+            .iter()
+            .flat_map(|line| {
+                let dos = do_regex.find_iter(line).map(|m| m.start());
+                let donts = dont_regex.find_iter(line).map(|m| m.start());
+
+                let mut do_or_dont = dos
+                    .map(|i| (i, true))
+                    .chain(donts.map(|i| (i, false)))
+                    .collect_vec();
+                do_or_dont.sort_unstable();
+
+                let mut ranges = Vec::new();
+                let mut last_idx = 0;
+                let mut last_do = true;
+                for (pos, is_do) in do_or_dont {
+                    match (last_do, is_do) {
+                        // if we receive the same instruction, just ignore it.  don't even store the
+                        // current index, just completely pretend this never happened.
+                        (true, true) | (false, false) => {
+                            continue;
+                        }
+                        // was do, but now we should don't
+                        (true, false) => {
+                            ranges.push(last_idx..pos);
+                        }
+                        // was don't, but now we should do... just fall through
+                        (false, true) => {}
+                    }
+
+                    last_idx = pos;
+                    last_do = is_do;
+                }
+
+                ranges.into_iter().flat_map(|idxs| {
+                    mul_regex.captures_iter(&line[idxs]).map(|c| {
+                        c.get(1).unwrap().as_str().parse::<u64>().unwrap()
+                            * c.get(2).unwrap().as_str().parse::<u64>().unwrap()
+                    })
+                })
+            })
+            .sum())
     }
 }
