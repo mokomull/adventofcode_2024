d7df8131423b75c3f41c59e7125672db0c6f7db7
Matt Mullins <mmullins@mmlx.us>
2024-12-02 21:10:35 -0800

    day 03: actually use the input

--- day_03/src/test.rs
+++ day_03/src/test.rs
@@ -2,11 +2,11 @@ use prelude::Day;
 
 use crate::Solution;
 
-static INPUT: &str = "";
+static INPUT: &str = include_str!("input.txt");
 
 #[test]
 fn day_03() {
     let solution = Solution::new(INPUT);
-    assert_eq!(0, solution.part1().unwrap());
+    assert_eq!(192767529, solution.part1().unwrap());
     assert_eq!(0, solution.part2().unwrap());
 }
