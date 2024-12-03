29c01395d1fa31529b9f57a533cb11d3f68e790a
Matt Mullins <mmullins@mmlx.us>
2024-12-02 21:24:39 -0800

    day 03: add a test for part 2

--- day_03/src/test.rs
+++ day_03/src/test.rs
@@ -10,3 +10,9 @@ fn day_03() {
     assert_eq!(192767529, solution.part1().unwrap());
     assert_eq!(0, solution.part2().unwrap());
 }
+
+#[test]
+fn example_part2() {
+    let solution = Solution::new("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))");
+    assert_eq!(48, solution.part2().unwrap());
+}
