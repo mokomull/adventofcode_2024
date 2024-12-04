e6540554d17beebfccd27a91207e171dc118ffef
Matt Mullins <mmullins@mmlx.us>
2024-12-03 21:48:26 -0800

    day 04: template

---
+++ day_04/src/lib.rs
@@ -0,0 +1,20 @@
+use prelude::Day;
+
+#[cfg(test)]
+mod test;
+
+pub struct Solution {}
+
+impl Day for Solution {
+    fn new(input: &str) -> Self {
+        todo!()
+    }
+
+    fn part1(&self) -> prelude::anyhow::Result<u64> {
+        todo!()
+    }
+
+    fn part2(&self) -> prelude::anyhow::Result<u64> {
+        todo!()
+    }
+}
