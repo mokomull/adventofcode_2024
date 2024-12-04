72a0e2a0c9b94330d0f6d8fe862fb5d6c3d0357f
Matt Mullins <mmullins@mmlx.us>
2024-12-03 22:12:37 -0800

    day 04: part 1 answer

--- day_04/src/test.rs
+++ day_04/src/test.rs
@@ -7,7 +7,7 @@ static INPUT: &str = include_str!("input.txt");
 #[test]
 fn day_04() {
     let solution = Solution::new(INPUT);
-    assert_eq!(0, solution.part1().unwrap());
+    assert_eq!(2593, solution.part1().unwrap());
     assert_eq!(0, solution.part2().unwrap());
 }
 
