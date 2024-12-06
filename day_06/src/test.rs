325d07ee6715b1d4468f33a64b0579d315e865a1
Matt Mullins <mmullins@mmlx.us>
2024-12-05 21:35:01 -0800

    day 06: template

---
+++ day_06/src/test.rs
@@ -0,0 +1,11 @@
+use crate::Solution;
+use prelude::Day;
+
+static INPUT: &str = "";
+
+#[test]
+fn day_06() {
+    let solution = Solution::new(INPUT);
+    assert_eq!(0, solution.part1().unwrap());
+    assert_eq!(0, solution.part2().unwrap());
+}
