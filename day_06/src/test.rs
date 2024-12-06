500b7b1d5414748f8b576611c7ce59d1bf26a954
Matt Mullins <mmullins@mmlx.us>
2024-12-05 22:20:50 -0800

    day 06: test with the example
    But this works!?

--- day_06/src/test.rs
+++ day_06/src/test.rs
@@ -9,3 +9,21 @@ fn day_06() {
     assert_eq!(5305, solution.part1().unwrap());
     assert_eq!(0, solution.part2().unwrap());
 }
+
+#[test]
+fn example() {
+    let solution = Solution::new(
+        "....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...",
+    );
+    assert_eq!(41, solution.part1().unwrap());
+    assert_eq!(6, solution.part2().unwrap());
+}
