8de39c6c7e4ccaa833d088e85ddf714bfaee7514
Matt Mullins <mmullins@mmlx.us>
2024-12-06 21:05:23 -0800

    day 07: parser

--- day_07/src/lib.rs
+++ day_07/src/lib.rs
@@ -3,11 +3,30 @@ mod test;
 
 use prelude::*;
 
-pub struct Solution {}
+pub struct Solution {
+    equations: Vec<(i64, Vec<i64>)>,
+}
 
 impl Day for Solution {
     fn new(input: &str) -> Self {
-        todo!()
+        let equations = input
+            .lines()
+            .map(|line| {
+                let Some((target, values)) = line.split_once(": ") else {
+                    panic!("\": \" not found");
+                };
+
+                let target = target.parse().expect("bad integer");
+                let values = values
+                    .split_ascii_whitespace()
+                    .map(|s| s.parse().expect("bad integer in values"))
+                    .collect();
+
+                (target, values)
+            })
+            .collect();
+
+        Self { equations }
     }
 
     fn part1(&self) -> anyhow::Result<u64> {
