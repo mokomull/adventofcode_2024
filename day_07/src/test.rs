da145189910d37de5275a24225227de354250e8c
Matt Mullins <mmullins@mmlx.us>
2024-12-06 22:02:56 -0800

    day 07: part 2 test results

--- day_07/src/test.rs
+++ day_07/src/test.rs
@@ -8,7 +8,7 @@ static INPUT: &str = include_str!("input.txt");
 fn day_07() {
     let solution = Solution::new(INPUT);
     assert_eq!(1153997401072, solution.part1().unwrap());
-    assert_eq!(0, solution.part2().unwrap());
+    assert_eq!(97902809384118, solution.part2().unwrap());
 }
 
 #[test]
