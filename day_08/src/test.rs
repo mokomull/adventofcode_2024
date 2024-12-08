f982e3a16d43d58d9fc26a3956c2b704b947bdf5
Matt Mullins <mmullins@mmlx.us>
2024-12-07 21:42:45 -0800

    day 08: commit part1 result

--- day_08/src/test.rs
+++ day_08/src/test.rs
@@ -7,7 +7,7 @@ static INPUT: &str = include_str!("input.txt");
 #[test]
 fn day_08() {
     let solution = Solution::new(INPUT);
-    assert_eq!(0, solution.part1().unwrap());
+    assert_eq!(376, solution.part1().unwrap());
     assert_eq!(0, solution.part2().unwrap());
 }
 
