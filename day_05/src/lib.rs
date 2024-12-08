7bb631d746b356203f431c0c99dad26ec93844b9
Matt Mullins <mmullins@mmlx.us>
2024-12-07 23:30:43 -0800

    day 05: parser

--- day_05/src/lib.rs
+++ day_05/src/lib.rs
@@ -3,11 +3,38 @@ mod test;
 
 use prelude::*;
 
-struct Solution {}
+struct Solution {
+    rules: HashMap<usize, HashSet<usize>>,
+    pages: Vec<Vec<usize>>,
+}
 
 impl Day for Solution {
     fn new(input: &str) -> Self {
-        todo!()
+        let mut lines = input.lines();
+
+        let mut rules: HashMap<usize, HashSet<usize>> = HashMap::new();
+        for line in &mut lines {
+            if line.is_empty() {
+                break;
+            }
+
+            let (before, after) = line.split_once('|').expect("no pipe found");
+            let before = before.parse().expect("bad integer in before");
+            let after = after.parse().expect("bad integer in after");
+
+            rules.entry(before).or_default().insert(after);
+        }
+
+        let mut pages = Vec::new();
+        for line in lines {
+            pages.push(
+                line.split(',')
+                    .map(|s| s.parse().expect("bad integer in pages"))
+                    .collect_vec(),
+            );
+        }
+
+        Self { rules, pages }
     }
 
     fn part1(&self) -> anyhow::Result<u64> {
