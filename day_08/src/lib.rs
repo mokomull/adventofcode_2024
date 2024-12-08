e95a9e516c080019cb48ed6cf9d07a3024e818ed
Matt Mullins <mmullins@mmlx.us>
2024-12-07 21:23:49 -0800

    day 08: I misunderstood the assignment
    Again.
    
    Nothing says that the antinode has to be *between* antennas, the equal
    distance could be arbitrarily far.

--- day_08/src/lib.rs
+++ day_08/src/lib.rs
@@ -5,38 +5,50 @@ use prelude::*;
 
 pub struct Solution {
     antennas: HashMap<u8, HashSet<(usize, usize)>>,
+    height: usize,
+    width: usize,
 }
 
 impl Day for Solution {
     fn new(input: &str) -> Self {
         let mut antennas: HashMap<u8, HashSet<(usize, usize)>> = HashMap::new();
+        let mut height = 0;
+        let mut width = 0;
 
         for (i, line) in input.lines().enumerate() {
+            height += 1;
+            width = line.len();
+
             for (j, c) in line.bytes().enumerate() {
                 if c != b'.' {
                     antennas.entry(c).or_default().insert((i, j));
                 }
             }
         }
-        Self { antennas }
+        Self {
+            antennas,
+            height,
+            width,
+        }
     }
 
     fn part1(&self) -> anyhow::Result<u64> {
         let mut antinodes = HashSet::new();
 
-        for (_frequency, locations) in self.antennas.iter() {
-            for (p, q) in locations.iter().cartesian_product(locations.iter()) {
-                if p == q {
-                    continue;
-                }
+        for i in 0..self.height {
+            for j in 0..self.width {
+                for (_frequency, locations) in self.antennas.iter() {
+                    for (p, q) in locations.iter().cartesian_product(locations.iter()) {
+                        if p == q {
+                            continue;
+                        }
 
-                // The antinode will be one-third of the way from p to q, which, in order to be
-                // on an integer grid, means that both differences will be multiples of 3.
-                let di = q.0 as i32 - p.0 as i32;
-                let dj = q.1 as i32 - p.1 as i32;
+                        let location = (i, j);
 
-                if di % 3 == 0 && dj % 3 == 0 {
-                    antinodes.insert((p.0 as i32 + (di / 3), p.1 as i32 + (dj / 3)));
+                        if distance_squared(location, *p) == distance_squared(location, *q) {
+                            antinodes.insert((i, j));
+                        }
+                    }
                 }
             }
         }
@@ -47,4 +59,12 @@ impl Day for Solution {
     fn part2(&self) -> anyhow::Result<u64> {
         todo!()
     }
+}
+
+fn distance_squared(a: (usize, usize), b: (usize, usize)) -> usize {
+    use std::cmp::{max, min};
+    let dx = max(a.0, b.0) - min(a.0, b.0);
+    let dy = max(a.1, b.1) - min(a.1, b.1);
+
+    dx * dx + dy * dy
 }
