4f33f870b1bcfb6cdd6127133bab95ce242672ac
Matt Mullins <mmullins@mmlx.us>
2024-12-07 21:13:00 -0800

    day 08: really naive solution
    This seems to under-count..

--- day_08/src/lib.rs
+++ day_08/src/lib.rs
@@ -22,7 +22,26 @@ impl Day for Solution {
     }
 
     fn part1(&self) -> anyhow::Result<u64> {
-        todo!()
+        let mut antinodes = HashSet::new();
+
+        for (_frequency, locations) in self.antennas.iter() {
+            for (p, q) in locations.iter().tuple_combinations() {
+                if p == q {
+                    continue;
+                }
+
+                // The antinode will be one-third of the way from p to q, which, in order to be
+                // on an integer grid, means that both differences will be multiples of 3.
+                let di = q.0 as i32 - p.0 as i32;
+                let dj = q.1 as i32 - p.1 as i32;
+
+                if di % 3 == 0 && dj % 3 == 0 {
+                    antinodes.insert((p.0 as i32 + (di / 3), p.1 as i32 + (dj / 3)));
+                }
+            }
+        }
+
+        Ok(antinodes.len() as u64)
     }
 
     fn part2(&self) -> anyhow::Result<u64> {
--- day_08/src/test.rs
+++ day_08/src/test.rs
@@ -10,3 +10,22 @@ fn day_08() {
     assert_eq!(0, solution.part1().unwrap());
     assert_eq!(0, solution.part2().unwrap());
 }
+
+#[test]
+fn example() {
+    let solution = Solution::new(
+        "......#....#
+...#....0...
+....#0....#.
+..#....0....
+....0....#..
+.#....A.....
+...#........
+#......#....
+........A...
+.........A..
+..........#.
+..........#.",
+    );
+    assert_eq!(14, solution.part1().unwrap());
+}
