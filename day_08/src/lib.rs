86af3c9619385ab049fdae312ac00c5472829f61
Matt Mullins <mmullins@mmlx.us>
2024-12-07 21:06:40 -0800

    day 08: parser

--- day_08/src/lib.rs
+++ day_08/src/lib.rs
@@ -3,11 +3,22 @@ mod test;
 
 use prelude::*;
 
-pub struct Solution {}
+pub struct Solution {
+    antennas: HashMap<u8, HashSet<(usize, usize)>>,
+}
 
 impl Day for Solution {
     fn new(input: &str) -> Self {
-        todo!()
+        let mut antennas: HashMap<u8, HashSet<(usize, usize)>> = HashMap::new();
+
+        for (i, line) in input.lines().enumerate() {
+            for (j, c) in line.bytes().enumerate() {
+                if c != b'.' {
+                    antennas.entry(c).or_default().insert((i, j));
+                }
+            }
+        }
+        Self { antennas }
     }
 
     fn part1(&self) -> anyhow::Result<u64> {
