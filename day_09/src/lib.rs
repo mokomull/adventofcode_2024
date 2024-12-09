652aabc7d8c01ec10f819b120488bc032c2ed1e0
Matt Mullins <mmullins@mmlx.us>
2024-12-08 21:09:47 -0800

    day 09: parser

--- day_09/src/lib.rs
+++ day_09/src/lib.rs
@@ -3,11 +3,30 @@ use prelude::*;
 #[cfg(test)]
 mod test;
 
-struct Solution {}
+struct Solution {
+    disk: Vec<Option<i32>>,
+}
 
 impl Day for Solution {
     fn new(input: &str) -> Self {
-        todo!()
+        let mut bytes = input.bytes();
+        let mut disk = Vec::new();
+
+        let mut file_id = 0;
+        loop {
+            let Some(c) = bytes.next() else {
+                break;
+            };
+            disk.append(&mut vec![Some(file_id); (c - b'0') as usize]);
+            file_id += 1;
+
+            let Some(c) = bytes.next() else {
+                break;
+            };
+            disk.append(&mut vec![None; (c - b'0') as usize]);
+        }
+
+        Self { disk }
     }
 
     fn part1(&self) -> anyhow::Result<u64> {
