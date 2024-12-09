cf1b3a96b2b366e9327a52542a5ebb2cdc3e43e9
Matt Mullins <mmullins@mmlx.us>
2024-12-08 21:42:33 -0800

    day 09: add example input
    This ... is probably getting stuck in an infinite loop somewhere...

--- day_09/src/test.rs
+++ day_09/src/test.rs
@@ -10,3 +10,10 @@ fn day_09() {
     assert_eq!(6301895872542, solution.part1().unwrap());
     assert_eq!(0, solution.part2().unwrap());
 }
+
+#[test]
+fn example() {
+    let solution = Solution::new("2333133121414131402");
+    assert_eq!(1928, solution.part1().unwrap());
+    assert_eq!(2858, solution.part2().unwrap());
+}
