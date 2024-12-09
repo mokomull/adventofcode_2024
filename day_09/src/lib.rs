268957468c3ea88c60001158492bd522627691cb
Matt Mullins <mmullins@mmlx.us>
2024-12-08 20:41:13 -0800

    day 09: template

---
+++ day_09/src/lib.rs
@@ -0,0 +1,20 @@
+use prelude::*;
+
+#[cfg(test)]
+mod test;
+
+struct Solution {}
+
+impl Day for Solution {
+    fn new(input: &str) -> Self {
+        todo!()
+    }
+
+    fn part1(&self) -> anyhow::Result<u64> {
+        todo!()
+    }
+
+    fn part2(&self) -> anyhow::Result<u64> {
+        todo!()
+    }
+}
