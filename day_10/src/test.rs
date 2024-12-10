1802c8b76cc70b1dcdbdc5069e7743439cc0ad66
Matt Mullins <mmullins@mmlx.us>
2024-12-09 21:47:16 -0800

    day 10: commit test vector

--- day_10/src/test.rs
+++ day_10/src/test.rs
@@ -7,7 +7,7 @@ static INPUT: &str = include_str!("input.txt");
 #[test]
 fn day_10() {
     let solution = Solution::new(INPUT);
-    assert_eq!(0, solution.part1().unwrap());
+    assert_eq!(593, solution.part1().unwrap());
     assert_eq!(0, solution.part2().unwrap());
 }
 
