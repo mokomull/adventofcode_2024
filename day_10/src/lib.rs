082c6d627ea2b45fa36d5c5e490d478f43fe7e12
Matt Mullins <mmullins@mmlx.us>
2024-12-09 21:02:51 -0800

    day 10: "parser"
    Parser that really does not a whole lot...

--- day_10/src/lib.rs
+++ day_10/src/lib.rs
@@ -3,11 +3,17 @@ mod test;
 
 use prelude::*;
 
-struct Solution {}
+struct Solution {
+    map: Vec<Vec<u8>>,
+}
 
 impl Day for Solution {
     fn new(input: &str) -> Self {
-        todo!()
+        let map = input
+            .lines()
+            .map(|line| line.bytes().map(|b| b - b'0').collect())
+            .collect();
+        Self { map }
     }
 
     fn part1(&self) -> anyhow::Result<u64> {
