494de925228858b6fce38d452b7388176305ef24
Matt Mullins <mmullins@mmlx.us>
2024-12-09 21:35:20 -0800

    day 10: pretend it's a real graph and use petgraph
    all-pairs-shortest-path takes like eight seconds to run on this, so ...
    this may have been a bad start.
    
    And of course there's the whole problem where it Does Not Work At All™.

--- day_10/src/lib.rs
+++ day_10/src/lib.rs
@@ -1,6 +1,14 @@
 #[cfg(test)]
 mod test;
 
+use petgraph::{
+    algo::floyd_warshall,
+    visit::{
+        Data, GraphBase, GraphProp, IntoEdgeReferences, IntoNodeIdentifiers, NodeCompactIndexable,
+        NodeCount, NodeIndexable,
+    },
+    Directed,
+};
 use prelude::*;
 
 struct Solution {
@@ -17,10 +25,107 @@ impl Day for Solution {
     }
 
     fn part1(&self) -> anyhow::Result<u64> {
-        todo!()
+        let all_pairs =
+            floyd_warshall(&self, |_| 1).expect("no negative weights in the first place");
+
+        let mut possible_trailheads = Vec::new();
+        let mut peaks = Vec::new();
+
+        for (i, row) in self.map.iter().enumerate() {
+            for (j, &height) in row.iter().enumerate() {
+                if height == 0 {
+                    possible_trailheads.push((i, j));
+                }
+                if height == 9 {
+                    peaks.push((i, j));
+                }
+            }
+        }
+
+        Ok(possible_trailheads
+            .into_iter()
+            .map(|trailhead| {
+                peaks
+                    .iter()
+                    .filter(|&&peak| all_pairs.contains_key(&(trailhead, peak)))
+                    .count()
+            })
+            .sum::<usize>() as u64)
     }
 
     fn part2(&self) -> anyhow::Result<u64> {
         todo!()
+    }
+}
+
+impl GraphBase for Solution {
+    type EdgeId = ((usize, usize), (usize, usize));
+    type NodeId = (usize, usize);
+}
+
+impl Data for Solution {
+    type NodeWeight = ();
+    type EdgeWeight = ();
+}
+
+impl GraphProp for Solution {
+    type EdgeType = Directed;
+}
+
+impl<'a> IntoNodeIdentifiers for &'a Solution {
+    type NodeIdentifiers = Box<dyn Iterator<Item = (usize, usize)> + 'a>;
+
+    fn node_identifiers(self) -> Self::NodeIdentifiers {
+        Box::new((0..self.map.len()).flat_map(|i| (0..self.map[i].len()).map(move |j| (i, j))))
+    }
+}
+
+impl NodeCount for Solution {
+    fn node_count(&self) -> usize {
+        self.map.len() * self.map[0].len()
+    }
+}
+
+impl NodeIndexable for Solution {
+    fn node_bound(&self) -> usize {
+        self.node_count()
+    }
+
+    fn to_index(&self, (i, j): Self::NodeId) -> usize {
+        i * self.map[0].len() + j
+    }
+
+    #[doc = r" Convert `i` to a node index. `i` must be a valid value in the graph."]
+    fn from_index(&self, x: usize) -> Self::NodeId {
+        (x / self.map[0].len(), x % self.map[0].len())
+    }
+}
+
+impl NodeCompactIndexable for Solution {}
+
+impl<'a> IntoEdgeReferences for &'a Solution {
+    type EdgeRef = ((usize, usize), (usize, usize), &'static ());
+
+    type EdgeReferences = Box<dyn Iterator<Item = Self::EdgeRef> + 'a>;
+
+    fn edge_references(self) -> Self::EdgeReferences {
+        Box::new(self.node_identifiers().flat_map(|(i, j)| {
+            let mut edges = vec![];
+            let one_step_up = self.map[i][j] + 1;
+            if i > 0 && self.map[i - 1][j] == one_step_up {
+                edges.push(((i, j), (i - 1, j), &()));
+            }
+            if j > 0 && self.map[i][j - 1] == one_step_up {
+                edges.push(((i, j), (i, j - 1), &()));
+            }
+            if i < self.map.len() - 1 && self.map[i + 1][j] == one_step_up {
+                edges.push(((i, j), (i + 1, j), &()));
+            }
+            if j < self.map[i].len() - 1 && self.map[i][j + 1] == one_step_up {
+                edges.push(((i, j), (i, j + 1), &()));
+            }
+
+            edges.into_iter()
+        }))
     }
 }
