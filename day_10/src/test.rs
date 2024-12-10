e5093bc0d7f94f438b98e72a5e7f67386e9d54e9
Matt Mullins <mmullins@mmlx.us>
2024-12-09 22:08:13 -0800

    day 10: part 2 test vector

--- day_10/src/test.rs
+++ day_10/src/test.rs
@@ -8,7 +8,7 @@ static INPUT: &str = include_str!("input.txt");
 fn day_10() {
     let solution = Solution::new(INPUT);
     assert_eq!(593, solution.part1().unwrap());
-    assert_eq!(0, solution.part2().unwrap());
+    assert_eq!(1192, solution.part2().unwrap());
 }
 
 #[test]
