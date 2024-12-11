f97968116bdcbfa7281a6bcd380603ed72fda5f1
Matt Mullins <mmullins@mmlx.us>
2024-12-10 23:42:22 -0800

    day 11: parser

--- day_11/src/lib.rs
+++ day_11/src/lib.rs
@@ -3,11 +3,18 @@ use prelude::*;
 #[cfg(test)]
 mod test;
 
-pub struct Solution {}
+pub struct Solution {
+    stones: Vec<u64>,
+}
 
 impl Day for Solution {
     fn new(input: &str) -> Self {
-        todo!()
+        Self {
+            stones: input
+                .split_ascii_whitespace()
+                .map(|s| s.parse().unwrap())
+                .collect(),
+        }
     }
 
     fn part1(&self) -> anyhow::Result<u64> {
