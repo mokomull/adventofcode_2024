7b56dc8b3f45b859741f57143e2a052da39df8d0
Matt Mullins <mmullins@mmlx.us>
2024-12-11 01:47:23 -0800

    day 11: re-implement in Rust
    insight: every stone that's created is handled completely separately, so
    instead of storing them all, simply evaluate each one all the way first.
    The only case that isn't tail-recursion is the split-into-two.

--- day_11/src/lib.rs
+++ day_11/src/lib.rs
@@ -18,10 +18,37 @@ impl Day for Solution {
     }
 
     fn part1(&self) -> anyhow::Result<u64> {
-        todo!()
+        Ok(self
+            .stones
+            .iter()
+            .map(|&s| count_stones(25, s) as u64)
+            .sum())
     }
 
     fn part2(&self) -> anyhow::Result<u64> {
         todo!()
     }
+}
+
+fn count_stones(remaining_count: u8, stone: u64) -> usize {
+    if remaining_count == 0 {
+        return 1;
+    }
+
+    if stone == 0 {
+        return count_stones(remaining_count - 1, 1);
+    }
+
+    let digits = stone.to_string();
+    if digits.len() % 2 == 0 {
+        return count_stones(
+            remaining_count - 1,
+            digits[..digits.len() / 2].parse().unwrap(),
+        ) + count_stones(
+            remaining_count - 1,
+            digits[digits.len() / 2..].parse().unwrap(),
+        );
+    }
+
+    count_stones(remaining_count - 1, stone * 2024)
 }
