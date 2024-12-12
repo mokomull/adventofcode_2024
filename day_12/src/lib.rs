be3a6893c432bd6fd590640b69877aee74020d96
Matt Mullins <mmullins@mmlx.us>
2024-12-11 23:11:50 -0800

    day 12: first naive pass
    This seems to be an infinite loop...

--- day_12/src/lib.rs
+++ day_12/src/lib.rs
@@ -15,7 +15,53 @@ impl Day for Solution {
     }
 
     fn part1(&self) -> anyhow::Result<u64> {
-        todo!()
+        let mut to_visit = (0..self.map.len())
+            .cartesian_product(0..self.map[0].len())
+            .collect::<HashSet<_>>();
+
+        let mut total = 0;
+
+        while !to_visit.is_empty() {
+            let mut perimeter = 0;
+            let mut area = 0;
+            let mut to_visit_this = HashSet::from([to_visit.iter().cloned().next().unwrap()]);
+
+            while !to_visit_this.is_empty() {
+                let this = to_visit_this.iter().cloned().next().unwrap();
+                area += 1;
+                to_visit.remove(&this);
+                to_visit_this.remove(&this);
+
+                let (i, j) = this;
+                if i > 0 && self.map[i - 1][j] == self.map[i][j] {
+                    to_visit_this.insert((i - 1, j));
+                } else {
+                    perimeter += 1;
+                }
+
+                if j > 0 && self.map[i][j - 1] == self.map[i][j] {
+                    to_visit_this.insert((i, j - 1));
+                } else {
+                    perimeter += 1;
+                }
+
+                if i < self.map.len() - 1 && self.map[i + 1][j] == self.map[i][j] {
+                    to_visit_this.insert((i + 1, j));
+                } else {
+                    perimeter += 1;
+                }
+
+                if j < self.map[i].len() && self.map[i][j + 1] == self.map[i][j] {
+                    to_visit_this.insert((i, j + 1));
+                } else {
+                    perimeter += 1;
+                }
+            }
+
+            total += area + perimeter;
+        }
+
+        Ok(total)
     }
 
     fn part2(&self) -> anyhow::Result<u64> {
