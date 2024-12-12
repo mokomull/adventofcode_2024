b5cb9eef963b4999e6c6bccf958ba1273a30daa4
Matt Mullins <mmullins@mmlx.us>
2024-12-11 22:59:44 -0800

    day 12: "parser"
    This one also does minimal work...

--- day_12/src/lib.rs
+++ day_12/src/lib.rs
@@ -3,11 +3,15 @@ use prelude::*;
 #[cfg(test)]
 mod test;
 
-pub struct Solution {}
+pub struct Solution {
+    map: Vec<Vec<u8>>,
+}
 
 impl Day for Solution {
     fn new(input: &str) -> Self {
-        todo!()
+        Self {
+            map: input.lines().map(|line| line.bytes().collect()).collect(),
+        }
     }
 
     fn part1(&self) -> anyhow::Result<u64> {
