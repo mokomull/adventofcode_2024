f8582789b898a32d2a969daac094c26504d3dac6
Matt Mullins <mmullins@mmlx.us>
2024-12-13 20:24:26 -0800

    day 13: implement the matrix math
    This just unrolls a RREF operation on the matrix.
    
    Sadly, it doesn't seem to *work*...

--- day_13/src/lib.rs
+++ day_13/src/lib.rs
@@ -1,4 +1,5 @@
 use prelude::*;
+use rational::extras::gcd;
 
 #[cfg(test)]
 mod test;
@@ -13,8 +14,79 @@ struct Machine {
 
 impl Machine {
     fn solve(&self) -> Option<Pair> {
-        todo!("matrix math, probably")
+        let matrix = [
+            [self.button_a.0, self.button_b.0, self.prize.0],
+            [self.button_a.1, self.button_b.1, self.prize.1],
+        ];
+        let result = integer_rref(matrix)?;
+
+        // if everything balances out and we have an identity matrix on the left side
+        if result[0][0] == 1 && result[0][1] == 0 && result[1][0] == 0 && result[1][1] == 1 {
+            Some(Pair(result[0][2], result[1][2]))
+        } else {
+            None
+        }
     }
+}
+
+type Matrix = [[i64; 3]; 2];
+fn integer_rref(mut matrix: Matrix) -> Option<Matrix> {
+    // eliminate the first column of the second row
+    let a_gcd = gcd(matrix[0][0] as i128, matrix[1][0] as i128) as i64;
+    let x_factor = matrix[1][0] / a_gcd;
+    let y_factor = matrix[0][0] / a_gcd;
+    for col in 0..3 {
+        matrix[1][col] = matrix[1][col] * y_factor - matrix[0][col] * x_factor;
+    }
+    assert_eq!(0, matrix[1][0]);
+
+    // if this also eliminated the second column, then we're either over- or under-constrained
+    if matrix[1][1]== 0 {
+        // just return None in both cases -- we don't actually care if it's unwinnable or if all
+        // integers are solutions, because we'd still end up spending (0, 0) tokens.
+        return None;
+    }
+
+    // make the leading term positive just because I forget how the % operator works in Rust ... I
+    // just know it's surprising.
+    if matrix[1][1] < 0 {
+        for col in 0..3 {
+            matrix[1][col] *= -1;
+        }
+    }
+
+    // eliminate the integer multiple across the second row
+    if matrix[1][2] % matrix[1][1] != 0 {
+        return None;
+    }
+    let divisor = matrix[1][1];
+    for col in 0..3 {
+        matrix[1][col] /= divisor;
+    }
+    assert_eq!(0, matrix[1][0]);
+    assert_eq!(1, matrix[1][1]);
+
+    // use the second row to back-substitude in the first row
+    let y_factor = matrix[0][1];
+    for col in 0..3 {
+        matrix[0][col] = matrix[0][col] - matrix[1][col] * y_factor;
+    }
+    assert_eq!(0, matrix[0][1]);
+
+    // in the general case we would also have to make the leading term of the top row positive here,
+    // but we know it *started* positive and it hasn't been changed yet.
+
+    // now eliminate the integer multiple across the top row
+    if matrix[0][2] % matrix[0][0] != 0 {
+        // also no integer solutions
+        return None;
+    }
+    let divisor = matrix[0][0];
+    for col in 0..3 {
+        matrix[0][col] /= divisor;
+    }
+
+    Some(matrix)
 }
 
 pub struct Solution {
