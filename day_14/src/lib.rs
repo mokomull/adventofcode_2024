60253f4eb5e6ae18531903a1055b7c373195e38c
Matt Mullins <mmullins@mmlx.us>
2024-12-13 21:09:37 -0800

    day 14: parser
    And I made all the coordinates i64 even though they're small, because
    there aren't that many robots and I don't want to think about integer
    limits.

--- day_14/src/lib.rs
+++ day_14/src/lib.rs
@@ -3,11 +3,33 @@ use prelude::*;
 #[cfg(test)]
 mod test;
 
-pub struct Solution {}
+struct Robot {
+    position: (i64, i64),
+    velocity: (i64, i64),
+}
+
+pub struct Solution {
+    robots: Vec<Robot>,
+}
 
 impl Day for Solution {
     fn new(input: &str) -> Self {
-        todo!()
+        let mut robots = Vec::new();
+
+        for line in input.lines() {
+            let (left, right) = line.split_once(' ').expect("no space");
+            let left = left.strip_prefix("p=").expect("no p=");
+            let right = right.strip_prefix("v=").expect("no v=");
+            let (px, py) = left.split_once(',').expect("no comma");
+            let (vx, vy) = right.split_once(',').expect("no comma");
+
+            robots.push(Robot {
+                position: (px.parse().unwrap(), py.parse().unwrap()),
+                velocity: (vx.parse().unwrap(), vy.parse().unwrap()),
+            })
+        }
+
+        Self { robots }
     }
 
     fn part1(&self) -> anyhow::Result<u64> {
