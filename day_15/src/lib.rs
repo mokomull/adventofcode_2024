3559d6d445ff4fa9a35eb40646ddfc482330a872
Matt Mullins <mmullins@mmlx.us>
2024-12-14 21:04:45 -0800

    day 15: "parser"

--- day_15/src/lib.rs
+++ day_15/src/lib.rs
@@ -3,11 +3,29 @@ use prelude::*;
 #[cfg(test)]
 mod test;
 
-pub struct Solution {}
+pub struct Solution {
+    map: Vec<Vec<u8>>,
+    moves: Vec<u8>,
+}
 
 impl Day for Solution {
     fn new(input: &str) -> Self {
-        todo!()
+        let mut lines = input.lines();
+
+        let mut map = Vec::new();
+        for line in &mut lines {
+            if line.is_empty() {
+                break;
+            }
+
+            map.push(line.into());
+        }
+        let mut moves = Vec::new();
+        for line in lines {
+            moves.extend_from_slice(line.trim().as_bytes());
+        }
+
+        Self { map, moves }
     }
 
     fn part1(&self) -> anyhow::Result<u64> {
