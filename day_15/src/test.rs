d64047ed119cf698ba0663b1efda0abb98240ae6
Matt Mullins <mmullins@mmlx.us>
2024-12-14 21:51:40 -0800

    day 15: add example to test suite for part 2

--- day_15/src/test.rs
+++ day_15/src/test.rs
@@ -8,3 +8,16 @@ fn day_15() {
     assert_eq!(1429911, solution.part1().unwrap());
     assert_eq!(0, solution.part2().unwrap());
 }
+
+#[test] fn example() {
+    let solution = Solution::new("#######
+#...#.#
+#.....#
+#..OO@#
+#..O..#
+#.....#
+#######
+
+<vv<<^^<<^^");
+    assert_eq!(9021, solution.part2().unwrap());
+}
