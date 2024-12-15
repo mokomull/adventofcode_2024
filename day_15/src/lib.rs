e5946d3a02f15238e0e14c7276f332cc76dda247
Matt Mullins <mmullins@mmlx.us>
2024-12-14 21:48:12 -0800

    day 15: slightly more complicated algo for part 2
    Very similar to the same logic, just complicated by the fan-out of extra
    work.

--- day_15/src/lib.rs
+++ day_15/src/lib.rs
@@ -29,7 +29,7 @@ impl Day for Solution {
     }
 
     fn part1(&self) -> anyhow::Result<u64> {
-        let mut map = self.map.clone();
+        let mut map: Vec<Vec<u8>> = self.map.clone();
         let mut robot = self
             .map
             .iter()
@@ -68,7 +68,59 @@ impl Day for Solution {
     }
 
     fn part2(&self) -> anyhow::Result<u64> {
-        todo!()
+        let mut map: Vec<Vec<u8>> = self
+            .map
+            .iter()
+            .map(|row| {
+                row.iter()
+                    .flat_map(|&c| -> [u8; 2] {
+                        match c {
+                            b'#' => *b"##",
+                            b'O' => *b"[]",
+                            b'.' => *b"..",
+                            b'@' => *b"@.",
+                            x => panic!("unexpected map tile {}", x),
+                        }
+                    })
+                    .collect()
+            })
+            .collect();
+
+        let mut robot = self
+            .map
+            .iter()
+            .enumerate()
+            .filter_map(|(i, line)| line.iter().position(|&c| c == b'@').map(|j| (i, j)))
+            .next()
+            .unwrap();
+
+        for &m in &self.moves {
+            let f = match m {
+                b'<' => |(i, j)| (i, j - 1),
+                b'>' => |(i, j)| (i, j + 1),
+                b'^' => |(i, j)| (i - 1, j),
+                b'v' => |(i, j)| (i + 1, j),
+                x => panic!("unexpected direction {:?}", x),
+            };
+            robot = wide_push(f, &mut map, robot);
+        }
+
+        Ok(map
+            .into_iter()
+            .enumerate()
+            .flat_map(|(i, row)| {
+                row.into_iter().enumerate().flat_map(
+                    move |(j, c)| {
+                        if c == b'[' {
+                            Some((i, j))
+                        } else {
+                            None
+                        }
+                    },
+                )
+            })
+            .map(|(i, j)| 100 * i + j)
+            .sum::<usize>() as u64)
     }
 }
 
@@ -105,4 +157,54 @@ where
     map[new_robot_i][new_robot_j] = b'@';
 
     (new_robot_i, new_robot_j)
+}
+
+// Returns the next position of the robot.  This is very similar to the above solution, except we
+// keep a list of work to do since one visit can create two squares to look at.
+fn wide_push<F>(step: F, map: &mut Vec<Vec<u8>>, robot: (usize, usize)) -> (usize, usize)
+where
+    F: Fn((usize, usize)) -> (usize, usize),
+{
+    let mut to_visit: Vec<(usize, usize)> = Vec::new();
+    let mut visited: HashSet<(usize, usize)> = HashSet::new();
+    to_visit.push(robot);
+    while let Some((i, j)) = to_visit.pop() {
+        if !visited.insert((i, j)) {
+            continue;
+        }
+
+        let (i, j) = step((i, j));
+        match map[i][j] {
+            b'[' => {
+                // still a stone so keep looking
+                to_visit.push((i, j));
+                to_visit.push((i, j + 1));
+            }
+            b']' => {
+                // other side of a stone
+                to_visit.push((i, j));
+                to_visit.push((i, j - 1));
+            }
+            b'#' => {
+                // a wall, so we can't move anything
+                return robot;
+            }
+            x => {
+                // this must be an empty space
+                assert_eq!(b'.', x);
+                break;
+            }
+        }
+    }
+
+    // everything that is in `visited` is a stone or the robot, and should be moved up
+    let old_map = map.clone();
+    for (i, j) in visited {
+        let (new_i, new_j) = step((i, j));
+        map[new_i][new_j] = old_map[i][j];
+    }
+    // and we just copied over the robot rather than overwriting it, so clear it out now
+    map[robot.0][robot.1] = b'.';
+
+    step(robot)
 }
