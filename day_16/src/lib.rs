ecdf7893f9a9c7444bbbbf7f0a06dc22f418a556
Matt Mullins <mmullins@mmlx.us>
2024-12-15 21:14:19 -0800

    day 16: "parse"

--- day_16/src/lib.rs
+++ day_16/src/lib.rs
@@ -3,11 +3,15 @@ use prelude::*;
 #[cfg(test)]
 mod test;
 
-struct Solution {}
+struct Solution {
+    map: Vec<Vec<u8>>,
+}
 
 impl Day for Solution {
     fn new(input: &str) -> Self {
-        todo!()
+        Self {
+            map: input.lines().map(|line| line.to_owned().into()).collect(),
+        }
     }
 
     fn part1(&self) -> anyhow::Result<u64> {
