e5d6c4cebefc400af83b95875364d67a52d51702
Matt Mullins <mmullins@mmlx.us>
2024-12-16 21:28:32 -0800

    day 17: implement the silly little computer

--- day_17/src/lib.rs
+++ day_17/src/lib.rs
@@ -1,3 +1,6 @@
+use core::panic;
+use std::ops::ControlFlow;
+
 use prelude::*;
 
 #[cfg(test)]
@@ -42,5 +45,83 @@ impl Day for Solution {
 
     fn part2(&self) -> anyhow::Result<u64> {
         todo!()
+    }
+}
+
+struct Computer {
+    a: u64,
+    b: u64,
+    c: u64,
+    program: Vec<u64>,
+    ip: usize,
+}
+
+impl From<&Solution> for Computer {
+    fn from(value: &Solution) -> Self {
+        Self {
+            a: value.a,
+            b: value.b,
+            c: value.c,
+            program: value.program.clone(),
+            ip: 0,
+        }
+    }
+}
+
+impl Computer {
+    fn step(&mut self) -> ControlFlow<(), Option<u64>> {
+        if self.ip >= self.program.len() {
+            return ControlFlow::Break(());
+        }
+
+        let mut output = None;
+        let mut dont_inc = false;
+        match self.program[self.ip] {
+            0 => {
+                self.a >>= self.this_combo();
+            }
+            1 => {
+                self.b = self.b ^ self.program[self.ip + 1];
+            }
+            2 => {
+                self.b = self.this_combo() % 8;
+            }
+            3 => {
+                if self.a == 0 {
+                    self.ip = self.program[self.ip + 1] as usize;
+                    dont_inc = true;
+                }
+            }
+            4 => {
+                self.b = self.b ^ self.c;
+            }
+            5 => {
+                output = Some(self.this_combo());
+            }
+            6 => {
+                self.b >>= self.this_combo();
+            }
+            7 => {
+                self.c >>= self.this_combo();
+            }
+
+            x => panic!("unexpected instruction {x}"),
+        }
+
+        if !dont_inc {
+            self.ip += 2;
+        }
+
+        ControlFlow::Continue(output)
+    }
+
+    fn this_combo(&self) -> u64 {
+        match self.program[self.ip + 1] {
+            x @ 0..=3 => x,
+            4 => self.a,
+            5 => self.b,
+            6 => self.c,
+            x => panic!("unexpected combo operand {x}"),
+        }
     }
 }
