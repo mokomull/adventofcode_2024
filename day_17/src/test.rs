9f3fdf1742180b208f78d5d5dd857de05ef4be13
Matt Mullins <mmullins@mmlx.us>
2024-12-16 21:33:38 -0800

    day 17: use the example data

--- day_17/src/test.rs
+++ day_17/src/test.rs
@@ -8,3 +8,15 @@ fn day_17() {
     assert_eq!("", &solution.part1().unwrap());
     assert_eq!(0, solution.part2().unwrap());
 }
+
+#[test]
+fn example() {
+    let solution = Solution::new(
+        "Register A: 729
+Register B: 0
+Register C: 0
+
+Program: 0,1,5,4,3,0",
+    );
+    assert_eq!("4,6,3,5,6,3,5,2,1,0", &solution.part1().unwrap())
+}
