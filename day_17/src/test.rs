30039970a58b17c54ab269625211e1aa0cb4fc28
Matt Mullins <mmullins@mmlx.us>
2024-12-16 22:03:46 -0800

    day 17: the part 2 example is a different program
    sneaky!

--- day_17/src/test.rs
+++ day_17/src/test.rs
@@ -19,5 +19,16 @@ Register C: 0
 Program: 0,1,5,4,3,0",
     );
     assert_eq!("4,6,3,5,6,3,5,2,1,0", &solution.part1().unwrap());
+}
+
+#[test]
+fn part2_example() {
+    let solution = Solution::new(
+        "Register A: 2024
+Register B: 0
+Register C: 0
+
+Program: 0,3,5,4,3,0",
+    );
     assert_eq!(117440, solution.part2().unwrap());
 }
