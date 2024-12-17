9569170ad17f2b1f6b96298cdfa882b4d29bc2bc
Matt Mullins <mmullins@mmlx.us>
2024-12-16 21:15:08 -0800

    day 17: parser

--- day_17/src/lib.rs
+++ day_17/src/lib.rs
@@ -3,11 +3,37 @@ use prelude::*;
 #[cfg(test)]
 mod test;
 
-pub struct Solution {}
+pub struct Solution {
+    a: u64,
+    b: u64,
+    c: u64,
+    program: Vec<u64>,
+}
 
 impl Day for Solution {
     fn new(input: &str) -> Self {
-        todo!()
+        let mut lines = input.lines();
+
+        let register_a = lines.next().unwrap();
+        let register_a = register_a.strip_prefix("Register A: ").unwrap();
+        let a = register_a.parse().unwrap();
+
+        let register_b = lines.next().unwrap();
+        let register_b = register_b.strip_prefix("Register B: ").unwrap();
+        let b = register_b.parse().unwrap();
+
+        let register_c = lines.next().unwrap();
+        let register_c = register_c.strip_prefix("Register C: ").unwrap();
+        let c = register_c.parse().unwrap();
+
+        let blank = lines.next().unwrap();
+        assert!(blank.is_empty());
+
+        let program = lines.next().unwrap();
+        let program = program.strip_prefix("Program: ").unwrap();
+        let program = program.split(',').map(|s| s.parse().unwrap()).collect();
+
+        Self { a, b, c, program }
     }
 
     fn part1(&self) -> anyhow::Result<u64> {
