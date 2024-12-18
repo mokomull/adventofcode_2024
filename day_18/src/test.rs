dbfc2063ff0a02aaf32e5af98053c0e9ab8a0fa8
Matt Mullins <mmullins@mmlx.us>
2024-12-17 22:02:38 -0800

    day 18: test the example bytes falling
    This... works?!  Dang!

--- day_18/src/test.rs
+++ day_18/src/test.rs
@@ -8,3 +8,36 @@ fn day_18() {
     assert_eq!(0, solution.part1().unwrap());
     assert_eq!(0, solution.part2().unwrap());
 }
+
+#[test]
+fn example() {
+    let solution = Solution::<12, 6>::new(EXAMPLE_INPUT);
+    assert_eq!(22, solution.part1().unwrap());
+}
+
+static EXAMPLE_INPUT: &str = "5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0
+";
