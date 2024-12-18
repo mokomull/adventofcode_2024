6fb9b36501b4e3ba92a0dbd21b4f5f5e8815cfb8
Matt Mullins <mmullins@mmlx.us>
2024-12-17 22:12:35 -0800

    day 18: factor out the breadth-first-search
    There's only 3,000 or so lines in my personal input, so I'm hoping just
    doing this n times will be fine...

--- day_18/src/lib.rs
+++ day_18/src/lib.rs
@@ -32,42 +32,46 @@ impl<const COUNT: usize, const DIMENSION: u8> Day for Solution<COUNT, DIMENSION>
 
         assert_eq!(COUNT, corrupted.len());
 
-        let mut to_visit = VecDeque::from([((0, 0), 0)]);
-        let mut visited = HashSet::new();
-
-        while let Some(((i, j), d)) = to_visit.pop_front() {
-            if !visited.insert((i, j)) {
-                // we've already been here
-                continue;
-            }
-
-            if corrupted.contains(&(i, j)) {
-                // we can't enter a corrupted square
-                continue;
-            }
-
-            if (i, j) == (DIMENSION, DIMENSION) {
-                return Ok(d);
-            }
-
-            if i > 0 {
-                to_visit.push_back(((i - 1, j), d + 1));
-            }
-            if j > 0 {
-                to_visit.push_back(((i, j - 1), d + 1));
-            }
-            if i < DIMENSION {
-                to_visit.push_back(((i + 1, j), d + 1));
-            }
-            if j < DIMENSION {
-                to_visit.push_back(((i, j + 1), d + 1));
-            }
-        }
-
-        anyhow::bail!("No path found :'(");
+        distance_to_end::<DIMENSION>(&corrupted)
     }
 
     fn part2(&self) -> anyhow::Result<u64> {
         todo!()
     }
+}
+
+fn distance_to_end<const DIMENSION: u8>(corrupted: &HashSet<(u8, u8)>) -> anyhow::Result<u64> {
+    let mut to_visit = VecDeque::from([((0, 0), 0)]);
+    let mut visited = HashSet::new();
+
+    while let Some(((i, j), d)) = to_visit.pop_front() {
+        if !visited.insert((i, j)) {
+            // we've already been here
+            continue;
+        }
+
+        if corrupted.contains(&(i, j)) {
+            // we can't enter a corrupted square
+            continue;
+        }
+
+        if (i, j) == (DIMENSION, DIMENSION) {
+            return Ok(d);
+        }
+
+        if i > 0 {
+            to_visit.push_back(((i - 1, j), d + 1));
+        }
+        if j > 0 {
+            to_visit.push_back(((i, j - 1), d + 1));
+        }
+        if i < DIMENSION {
+            to_visit.push_back(((i + 1, j), d + 1));
+        }
+        if j < DIMENSION {
+            to_visit.push_back(((i, j + 1), d + 1));
+        }
+    }
+
+    anyhow::bail!("No path found :'(");
 }
