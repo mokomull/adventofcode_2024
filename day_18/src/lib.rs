0d26a00a250595db9a9dba3e10c7aaadd1bfeeac
Matt Mullins <mmullins@mmlx.us>
2024-12-18 00:07:35 -0800

    day 18: use DFS for the part 2 path
    It actually doesn't matter *how* good the path actually is, just that it
    exists.  `dfs_end` is essentially the same as `find_end`, except it now
    just pushes and pops to/from the end of a Vec -- we don't need the Deque
    anymore.
    
    Now we're down from 0.2s to 0.08s because it doesn't spend time
    accumulating middle nodes for potential short paths.

--- day_18/src/lib.rs
+++ day_18/src/lib.rs
@@ -43,7 +43,7 @@ impl<const COUNT: usize, const DIMENSION: u8> Solution<COUNT, DIMENSION> {
             corrupted.insert((i, j));
 
             if last_path.is_none() || last_path.as_ref().unwrap().contains(&(i, j)) {
-                match find_end::<DIMENSION>(&corrupted) {
+                match dfs_end::<DIMENSION>(&corrupted) {
                     Err(_) => return Ok(format!("{i},{j}")),
                     Ok(p) => last_path = Some(p),
                 }
@@ -86,6 +86,44 @@ fn find_end<const DIMENSION: u8>(corrupted: &HashSet<(u8, u8)>) -> anyhow::Resul
         }
         if j < DIMENSION {
             to_visit.push_back(((i, j + 1), p));
+        }
+    }
+
+    anyhow::bail!("No path found :'(");
+}
+
+fn dfs_end<const DIMENSION: u8>(corrupted: &HashSet<(u8, u8)>) -> anyhow::Result<Vec<(u8, u8)>> {
+    let mut to_visit = vec![((0, 0), vec![])];
+    let mut visited = HashSet::new();
+
+    while let Some(((i, j), mut p)) = to_visit.pop() {
+        if !visited.insert((i, j)) {
+            // we've already been here
+            continue;
+        }
+
+        if corrupted.contains(&(i, j)) {
+            // we can't enter a corrupted square
+            continue;
+        }
+
+        if (i, j) == (DIMENSION, DIMENSION) {
+            return Ok(p);
+        }
+
+        p.push((i, j));
+
+        if i > 0 {
+            to_visit.push(((i - 1, j), p.clone()));
+        }
+        if j > 0 {
+            to_visit.push(((i, j - 1), p.clone()));
+        }
+        if i < DIMENSION {
+            to_visit.push(((i + 1, j), p.clone()));
+        }
+        if j < DIMENSION {
+            to_visit.push(((i, j + 1), p));
         }
     }
 
